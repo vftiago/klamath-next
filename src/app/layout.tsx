7dc56727233d03efb2e1e24cc5578a44862d01e4
vftiago <vf.tiago@gmail.com>
2025-01-03 17:27:54 +0000

    📈 enable vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 
 import DynamicThreeScene from "@/app/_3d/DynamicThreeScene";
-
 const robotoCondensed = Roboto_Condensed({
   subsets: ["latin"],
   variable: "--font-roboto-condensed",
@@ -25,6 +26,8 @@ const RootLayout = ({
       <body className={`${robotoCondensed.variable} size-full font-sans antialiased`}>
         <DynamicThreeScene />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
