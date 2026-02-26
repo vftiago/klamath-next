04fb7ee1d40613c31182e96becbd0de0d574d1a4
vftiago <vf.tiago@gmail.com>
2026-02-26 20:20:56 +0000

    🐛 add page revalidation

--- src/app/(routes)/(sub-routes)/projects/page.tsx
+++ src/app/(routes)/(sub-routes)/projects/page.tsx
@@ -1,6 +1,8 @@
 import DataPageLayout from "@/app/_shared/ui/data-page-layout";
 import ProjectData from "./project-data";
 
+export const revalidate = 3600;
+
 const ProjectPage = () => {
   return (
     <DataPageLayout title="Projects">
--- src/app/(routes)/(sub-routes)/repositories/page.tsx
+++ src/app/(routes)/(sub-routes)/repositories/page.tsx
@@ -1,6 +1,8 @@
 import DataPageLayout from "@/app/_shared/ui/data-page-layout";
 import RepositoryData from "./repository-data";
 
+export const revalidate = 3600;
+
 const RepositoryPage = () => {
   return (
     <DataPageLayout title="Repositories">
